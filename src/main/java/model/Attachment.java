dd93b4851de57ef2ceb202adbe6d09ac219d834a
Kamlesh Delat <kamlesh.delat@symphony.com>
2020-08-26 20:05:32 +0530

    Add images field for attachment

--- src/main/java/model/Attachment.java
+++ src/main/java/model/Attachment.java
@@ -1,44 +1,55 @@
 package model;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Attachment {
 
-    private String id;
-    private String name;
-    private Long size;
-    private ImageInfo image;
+  private String id;
+  private String name;
+  private Long size;
+  private ImageInfo image;
+  private List<ImageInfo> images;
 
-    public String getId() {
-        return id;
-    }
+  public String getId() {
+    return id;
+  }
 
-    public void setId(String id) {
-        this.id = id;
-    }
+  public void setId(String id) {
+    this.id = id;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public Long getSize() {
-        return size;
-    }
+  public Long getSize() {
+    return size;
+  }
 
-    public void setSize(Long size) {
-        this.size = size;
-    }
+  public void setSize(Long size) {
+    this.size = size;
+  }
 
-    public ImageInfo getImage() {
-        return image;
-    }
+  public ImageInfo getImage() {
+    return image;
+  }
 
-    public void setImage(ImageInfo image) {
-        this.image = image;
-    }
+  public void setImage(ImageInfo image) {
+    this.image = image;
+  }
+
+  public List<ImageInfo> getImages() {
+    return images;
+  }
+
+  public void setImages(List<ImageInfo> images) {
+    this.images = images;
+  }
 }
